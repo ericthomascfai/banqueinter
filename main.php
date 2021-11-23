a5753b05fd54ce5782d3551a5296843bbdf6bfc8
eric <eric.thomas@formation-industries-fc.fr>
2021-11-23 14:43:07 +0100

    v3

--- main.php
+++ main.php
@@ -1,12 +1,29 @@
 <?php
 require("Compte.php");
 require("Client.php");
-
 $compte=new Compte(1);
+
 if(isset($_POST["depot"]))
 {
     $compte->depot($_POST["depot"]);
+    echo "<script>alert('Dépot effectué'); document.location='menucompte.html';</script>";
 }
+else
+    if(isset($_POST["retrait"]))
+    {
+        $compte->retrait($_POST["retrait"]);
+        echo "<script>alert('Retrait effectué'); document.location='menucompte.html';</script>";
+    }
+    else
+        if(isset($_POST["destinataire"])&&isset($_POST["montantvirement"]))
+        {
+            $compte->virer($_POST["montantvirement"],$_POST["destinataire"]);
+            echo "<script>alert('Virement effectué'); document.location='menucompte.html';</script>";
+        }
+        else
+        {
+            $compte->afficherSolde();
+        }
 
 
 
